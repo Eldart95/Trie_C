d046ec44ec2d7c8bfc91c38e28175d93c6144847
AlexMankovetski <57401039+AlexMankovetski@users.noreply.github.com>
2020-01-14 13:41:45 +0200

    forgot

---
+++ Trie.h
@@ -0,0 +1,8 @@
+#ifndef _FIND_H_
+#define _FIND_H_
+
+void insert(struct node *root, const char *key);
+bool search(struct node *root, const char *key);
+void printWord(char cahrs[],int len, int count);
+
+#endif
